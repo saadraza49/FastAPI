a7f594cb8e3a3abafc2721728ef217373ffdaf93
Muhammad Saad Raza <msaadraza49@gmail.com>
2026-02-12 03:01:01 +0500

    Optional Key value and Converting  to dict

--- test.py
+++ test.py
@@ -1,18 +1,21 @@
 from fastapi import FastAPI, Body
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()           
 
-class Post(BaseModel):
-    title: str
+class Post(BaseModel):         # This is a model class
+    title: str                 # These are the required fields
     content: str
+    published: bool = True              
+    rating : Optional[int] = None
 
-
-@app.get("/")
+@app.get("/")                  # This is a get request
 def read():
     return {"message" : "Hello World"}
 
-@app.post("/create")
-def create_post(post: Post):
-    print(post.content)
-    return {"data" : "Post Created Successfully"}
+@app.post("/create")           # This is a post request
+def create_post(post: Post ):
+    print(post)
+    print(post.dict())         # Converts the post object to a dictionary
+    return {"data" : post}
