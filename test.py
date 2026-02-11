fe7c39281aa9f4226f62e9f8223562003888438c
Muhammad Saad Raza <msaadraza49@gmail.com>
2026-02-12 02:28:19 +0500

    Making class model

--- test.py
+++ test.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 app = FastAPI()           
+
+class Post(BaseModel):
+    title: str
+    content: str
+
 
 @app.get("/")
 def read():
     return {"message" : "Hello World"}
 
 @app.post("/create")
-def create_post(post: dict = Body(...)):
-    print(post)
-    return {"message" : "Post Created Successfully"}
+def create_post(post: Post):
+    print(post.content)
+    return {"data" : "Post Created Successfully"}
