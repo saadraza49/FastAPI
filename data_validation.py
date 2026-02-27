147865f8a281d3188917f53b1607e7173c88fc84
Muhammad Saad Raza <msaadraza49@gmail.com>
2026-02-27 17:51:27 +0500

    password field_validator

--- data_validation.py
+++ data_validation.py
@@ -9,16 +9,16 @@ class User(BaseModel):
     username: str = Field(..., min_length=2, max_length=50)
     age: int = Field(..., gt=0, lt=60)
     email : EmailStr
-    cgpa : float = Field(..., gt=0, lt=4.0)
+    cgpa : float = Field(..., gt=0.00, lt=4.00)
     married : Optional[bool] = Field(default=False)
     salary: int = Field(..., gt=0)
 
 
+@app.get("/")
+def home():
+    return "home"
 
 @app.post("/user/create")
 def create_user(user : User):
     return {"user" : user} 
 
-@app.get("/")
-def home():
-    return "home"
